2d96e266742635c5437efb8b3f9479a01fbb5929
Manuel Manzano <manuelalejandromanzano@gmail.com>
2022-01-18 22:40:28 -0500

    Create Variables.sol

---
+++ Variables.sol
@@ -0,0 +1,11 @@
+// testing variables with Uint
+pragma solidity ^0.5.13;
+
+contract WorkingWithVariables {
+    uint256 public myUint;
+
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+
+}
